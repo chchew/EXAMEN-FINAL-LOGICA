56073abcbb10b17280fa2d5ba61f7b61c26b5a72
Josue Valenzuela <val171001@uvg.edu.gt>
2019-11-19 18:16:15 -0600

    INIT

--- verify.py
+++ verify.py
@@ -1,6 +1,6 @@
 #Kevin Macario 17369
-#David Valenzuela
-#Carlos Chew
+#David Valenzuela 171001
+#Carlos Chew 17507
 
 #PROYECTO LOGICA MATEMATICA
 #MAQUINA DE TURING
@@ -10,6 +10,11 @@ import re
 
 mt1 = '00010001010000101000110000100010000100010001100001001000001000100110000101000001010011000001000100000100010011000001010101011100010001001000101'
 
+class sigma(object):
+
+    def __init__():
+        """
+        """
 
 def is_valid_input(Tu):
     """
@@ -17,18 +22,19 @@ def is_valid_input(Tu):
         - Tu: cadena e(T)e(z)
     returns: true if input is valid, false otherwise
     """
-    regex = '^0001(?:(?:(?:0{1,5})1(?:0{1,3})1(?:0{1,5})1(?:0{1,3})1(?:0{0,3})1)1)+1(?:0{1,3}1)+$'
+    regex = '^(?:0+)1(?:(?:(?:0+)1(?:0+)1(?:0+)1(?:0+)1(?:0+)1)1)+1(?:0+1)+$'
     match = re.search(regex, Tu)
     return match != None
 
+#Step 1
 def split_tape(Tu):
     """
     params:
         - Tu: cadena e(T)e(z)
     returns: tuple(tape1, tape2)
     """
-    reT = '^0001(?:(?:(?:0{1,5})1(?:0{1,3})1(?:0{1,5})1(?:0{1,3})1(?:0{0,3})1)1)+'
-    reZ = '1(?:0{1,3}1)+$'
+    reT = '^(?:0+)1(?:(?:(?:0+)1(?:0+)1(?:0+)1(?:0+)1(?:0+)1)1)+'
+    reZ = '1(?:0+1)+$'
     matchET = re.search(reT, Tu)
     matchEZ = re.search(reZ, Tu)
     return (matchET.group(0), '0'+matchEZ.group(0))
@@ -43,7 +49,37 @@ def readTxt(name):
             cadena.append(myline1)
         return cadena
 
+def get_entries(tape2):
+    """
+    params:
+        -tape2: Tape No 2 with entry string
+        -returns: List with the inputs
+    """
+    regex = '(?:^01)(.*)(?:1)'
+    match = re.search(regex, tape2)
+    return match.group(0).split('1')
+
+def print_tapes(t1, t2, t3):
+    print('tape 1:\n', t1)
+    print('tape 2:\n', t2)
+    print('tape 3:\n', t3)
+
+def print_line():
+    print('-'*50)
+    
+
 if __name__ == "__main__":
+    #step 1
     if(is_valid_input(mt1)):
+        print('Step 1:')
+        tape1 = mt1
+        tape2 = 'UqU'
+        tape3 = 'UqU'
+        print_tapes(tape1, tape2, tape3)
+        print_line()
+        #Step 2
+        print('Step 2:')
         tapes = split_tape(mt1)
-        print(tapes)
+        tape1, tape2 = split_tape(mt1)
+        print_tapes(tape1, tape2, tape3)
+
